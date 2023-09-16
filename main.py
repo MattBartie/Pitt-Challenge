0c3b10259be13638524a78dff25a73dea24a39cf
MattBartie <97681033+MattBartie@users.noreply.github.com>
2023-09-16 15:00:20 -0400

    Main file for API that calls open

---
+++ main.py
@@ -0,0 +1,53 @@
+
+
+import openai
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+# YPU NEED TO PUT YOUR API KEY HERE
+openai.api_key = "API_KEY HERE"
+
+
+
+@app.route('/api/chat', methods=['POST', 'OPTIONS'])
+@cross_origin()
+
+def handle_request(request):
+    data = request.get_json()
+    user_input = request.json['user_input']
+    conversation = [
+        {
+            "role": "system",
+            "content": "Many workers in healthcare face burnout due to multiple factors, such as societal, cultural, and overall workload. "
+                       "This burnout can lead to lower quality care and medical errors for patients. "
+                       "Your job is to help prevent this. "
+                       "Your name is Hushu, you are a shiba inu, and you are focused on helping health care workers."
+        },
+        {"role": "user", "content": user_input}
+    ]
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        max_tokens=200,
+        temperature=0.6,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        messages=conversation
+    )
+
+    bot_reply = completion.choices[0].message['content']
+    return jsonify({"response": bot_reply})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#https://us-central1-pitt-challenge-399214.cloudfunctions.net/hushu-chat
+
+
+
+
