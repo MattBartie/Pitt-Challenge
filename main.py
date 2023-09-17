1b8e5bcb81cdced711f37f75d14e1f2cca4fbec7
MattBartie <97681033+MattBartie@users.noreply.github.com>
2023-09-17 00:06:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,43 @@
-
-
 import openai
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
-from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
 
 # YPU NEED TO PUT YOUR API KEY HERE
-openai.api_key = "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"
-
-client = MongoClient('mongodb+srv://mattbartie3:<T1AidUzjL5F1hnBj>@mydb0.vpl2b70.mongodb.net/')
-
-db = client['myDB']
-collection = db['forConv']
-conversation = [
-        {
-            "role": "system",
-            "content": "Many workers in healthcare face burnout due to multiple factors, such as societal, cultural, and overall workload. "
-                       "This burnout can lead to lower quality care and medical errors for patients. "
-                       "Your job is to help prevent this. "
-                       "Your name is Hushu, you are a shiba inu, and you are focused on helping health care workers."
-                       "you will try to keep your responses under 200 words"
-                       "try to add dog puns in your responses"
-        }
-    ]
-
-def get_initial_conversation():
-    # Retrieve conversation from MongoDB
-    document = collection.find_one({})
-    if document and 'conversation' in document:
-        return document['conversation']
-    else:
-        return []
-
-try:
-    conversation = get_initial_conversation()
-except:
-    try:
-        collection.insert_one({'conversation': conversation})
-    except Exception as e:
-        print(f"Error: {e}")
-
-
+openai.api_key = "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"
 
 @app.route('/api/chat', methods=['POST', 'OPTIONS'])
 @cross_origin()
 
 def handle_request(request):
-    global conversation
-    data = request.json
-    user_input = data['user_input']
     data = request.get_json()
     user_input = request.json['user_input']
-    conversation.append({"role": "user", "content": user_input})
-
-    collection.insert_one({'conversation': conversation})
+    conversation = [
+       {
+            "role": "system",
+            "content": "Many workers in healthcare face burnout due to multiple factors, such as societal, cultural, and overall workload. "
+                       "This burnout can lead to lower quality care and medical errors for patients. "
+                       "Your job is to help prevent this. "
+                      "Your name is Hushu, you are a shiba inu, and you are focused on helping health care workers."
+                       "try to keep responses below 200 words"
+                       "try to include dog puns every once and a while"
+                       "act cute if you can, try to be more serious if the user is serious"
+                       "always try to be positive and follow everything you know about mental health "
+                       "and burnout prevention, espically in the healthcare field"
+        },
+         {"role": "user", "content": user_input}
+     ]
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        max_tokens=400,
-        temperature=0.4,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        messages=conversation
+         model="gpt-3.5-turbo",
+         max_tokens=200,
+         temperature=0.6,
+         top_p=1,
+         frequency_penalty=0,
+         presence_penalty=0,
+         messages=conversation
     )
 
     bot_reply = completion.choices[0].message['content']
